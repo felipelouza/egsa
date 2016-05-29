92780879a8f5e31abc3a77f84c1dc883bc603411
Felipe A. Louza <louza@localhost.localdomain>
2016-05-29 12:37:58 -0300

    Added external/convert_fasta.c

---
+++ external/convert_fasta.c
@@ -0,0 +1,71 @@
+/*
+ * Felipe A. Louza, 29/06/2016
+ * Remove header from .fasta file.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**********************************************************************/
+
+int main(int argc, char **argv) {
+	
+	//argv[1] = DIR
+	//argv[2] = DATABASE
+
+/**/
+	char* oldwd = getcwd(NULL,0);
+	if (!oldwd) return 1;
+	if (chdir(argv[1]) == -1) return 1;
+	free(oldwd);
+/**/
+	
+	FILE *f_in=NULL;	
+	f_in = fopen(argv[2], "r");		
+	if (!f_in) perror ("write_sequence");
+
+	
+	char *buf = NULL;
+	size_t len = 0;
+	
+	getline(&buf, &len, f_in);// first sequence '>'
+	free(buf);
+
+	char *c_buffer;
+	while(!feof(f_in)){
+		
+		len = 0;
+		int nalloc = 128;
+		c_buffer = (char*) malloc(nalloc*sizeof(char));
+
+		size_t p=0;
+		while(getline(&buf, &len, f_in)!=-1){
+
+			if(buf[0] == '>'){// break;
+				printf("%s", c_buffer);
+				break;
+			}
+
+			if(p+len>nalloc){
+				nalloc += 128;
+				c_buffer= realloc(c_buffer, sizeof(char) * nalloc);
+			}
+			strcpy(&c_buffer[p], buf);
+			p+=strlen(buf)-1;
+
+//			printf("%s", buf);
+		}
+		free(buf);
+		free(c_buffer);
+	}
+
+	printf("%s", c_buffer);
+	
+	fclose(f_in);
+
+return 0;
+}
+
