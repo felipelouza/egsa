73aaf4eef4fd44ea4bcda2f9951175959dfbdef8
Felipe A. Louza <louza@ic.unicamp.br>
2016-11-15 11:12:21 -0200

    Updated lib/settings/

--- lib/settings/defines_3.h
+++ lib/settings/defines_3.h
@@ -5,6 +5,12 @@
 
 #define MB	1048576
 
+#ifndef MEMLIMIT
+	#define MEMLIMIT 2048 //2GB of available RAM
+#endif
+
+#define RAM_USE MEMLIMIT*1024*1024LLU
+
 #ifndef DEBUG
 	#define DEBUG	0
 #endif
@@ -14,8 +20,11 @@
 #else
 	#define BWT_OUTPUT 1
 #endif
+	
+#define	IO_VOLUME	1
 
-#define BLOCK_ESA_SIZE 	(1024*MB)/sizeof(t_ESA)	// (10MB) size of buffer (BLOCK_ESA_SIZE > 0)
+//#define BLOCK_ESA_SIZE (1024*MB)/sizeof(t_ESA)	// size of ALL buffers (BLOCK_ESA_SIZE > 0)
+#define BLOCK_ESA_SIZE 	(size_t)(0.9*RAM_USE)/sizeof(t_ESA)	// size of ALL buffers (BLOCK_ESA_SIZE > 0)
 
 #define FILE_NAME	512
 #define FILE_READ	512		// bytes read from disk once
@@ -23,7 +32,8 @@
 /**********************************************************************/
 
 #define _OUTPUT_BUFFER	1
-#define OUTPUT_SIZE 	(64*MB)/sizeof(t_GSA) 		// size of output buffer
+//#define OUTPUT_SIZE 	(64*MB)/sizeof(t_GSA) 		// size of output buffer
+#define OUTPUT_SIZE 	(size_t)(0.06*RAM_USE)/sizeof(t_GSA) 		// size of output buffer
 
 /**/
 
@@ -34,7 +44,8 @@
 /**/
 
 #define INDUCED_BUFFER	1
-#define INDUCED_SIZE 	(16*MB)/sizeof(t_INDUCED) // 8 bytes per entry 1MB //2048: size of induced buffer (16Mb) 
+//#define INDUCED_SIZE 	(16*MB)/sizeof(t_INDUCED) // 8 bytes per entry 1MB //2048: size of induced buffer (16Mb) 
+#define INDUCED_SIZE 	(size_t)(0.02*RAM_USE)/sizeof(t_INDUCED) // 8 bytes per entry 1MB //2048: size of induced buffer (16Mb) 
 
 /**********************************************************************/
 
